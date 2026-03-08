393b04a8b90d37f4e4ca5e1bd9c1f5a188c096aa
Roman <just1newgames@gmail.com>
2026-03-08 20:20:03 +0300

    cpp commit. Need to add dragging.

--- cpp/LR-1/task2/main.cpp
+++ cpp/LR-1/task2/main.cpp
@@ -1,11 +1,10 @@
 #include <QApplication>
-#include "mainwindow/mainwindow.h"
+#include "mainwindow.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     QApplication app(argc, argv);
-
-    MainWindow window;
-    window.show();
-
+    MainWindow w;
+    w.show();
     return app.exec();
 }
