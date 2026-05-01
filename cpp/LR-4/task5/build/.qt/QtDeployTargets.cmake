9ccd27f1424a0c8cb9023eed9c82e87be46814a8
Roman <just1games@gmail.com>
2026-05-01 09:13:15 +0300

    Lr-4

--- cpp/LR-4/task5/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_main_FILE C:/projects/s2-lr/cpp/LR-4/task5/build/main.exe)
-set(__QT_DEPLOY_TARGET_main_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_main_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
